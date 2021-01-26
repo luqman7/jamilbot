cacc0338338546530dc1b60164040fe9cfd0ba21
Muhamad Luqman <muhamadluqmanhakim69@gmail.com>
2021-01-27 01:20:01 +0800

    v1

--- database/configDB.php
+++ database/configDB.php
@@ -3,12 +3,13 @@
 function connDB()
 {
 
+    $url = "postgres://buulugaexiflfy:c9232749191e959e79ce0a0fd1b806a7bb835354b6d391dfd54b596c2a750f92@ec2-54-235-158-17.compute-1.amazonaws.com:5432/d8h7sjoiqa1il";
     $host        = "host = ec2-54-235-158-17.compute-1.amazonaws.com";
     $port        = "port = 5432";
     $dbname      = "dbname = d8h7sjoiqa1il";
     $credentials = "user = buulugaexiflfy password=c9232749191e959e79ce0a0fd1b806a7bb835354b6d391dfd54b596c2a750f92";
 
-    $db = pg_connect("$host $port $dbname $credentials");
+    $db = pg_connect("$url $host $port $dbname $credentials");
     if (!$db) {
         echo "Error : Unable to open database\n";
     } else {
