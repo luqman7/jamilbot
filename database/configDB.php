d6d3dd801e50a5315821adc71739edfe7c1724f4
Muhamad Luqman <muhamadluqmanhakim69@gmail.com>
2021-01-27 00:05:24 +0800

    v1

--- database/configDB.php
+++ database/configDB.php
@@ -3,18 +3,17 @@
 function connDB()
 {
 
-    $dbServer = '';
-    $dbUser = 'buulugaexiflfy';
-    $dbPass = 'c9232749191e959e79ce0a0fd1b806a7bb835354b6d391dfd54b596c2a750f92';
-    $dbName = "d8h7sjoiqa1il";
+    $host        = "host = ec2-54-235-158-17.compute-1.amazonaws.com";
+    $port        = "port = 5432";
+    $dbname      = "dbname = d8h7sjoiqa1il";
+    $credentials = "user = buulugaexiflfy password=c9232749191e959e79ce0a0fd1b806a7bb835354b6d391dfd54b596c2a750f92";
 
-    $conn = mysqli_connect($dbServer, $dbUser, $dbPass);
-
-    if (!$conn) {
-        die('Conn fail: ' . mysqli_error($dbServer, $dbUser, $dbPass));
+    $db = pg_connect("$host $port $dbname $credentials");
+    if (!$db) {
+        echo "Error : Unable to open database\n";
+    } else {
+        echo "Opened database successfully\n";
     }
 
-    mysqli_select_db($conn, $dbName);
-
-    return $conn;
+    return $db;
 }
