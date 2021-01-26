88a304277d32f8bf7c594bd4fbea12170e35ed82
Muhamad Luqman <muhamadluqmanhakim69@gmail.com>
2021-01-27 00:00:19 +0800

    v1

--- database/configDB.php
+++ database/configDB.php
@@ -3,10 +3,10 @@
 function connDB()
 {
 
-    $dbServer = 'localhost';
-    $dbUser = 'root';
-    $dbPass = '';
-    $dbName = "fyp";
+    $dbServer = '';
+    $dbUser = 'buulugaexiflfy';
+    $dbPass = 'c9232749191e959e79ce0a0fd1b806a7bb835354b6d391dfd54b596c2a750f92';
+    $dbName = "d8h7sjoiqa1il";
 
     $conn = mysqli_connect($dbServer, $dbUser, $dbPass);
 
