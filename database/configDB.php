fc512fda0b2575a03a0f1a427a9c2768293a9acf
Muhamad Luqman <muhamadluqmanhakim69@gmail.com>
2021-01-27 09:43:19 +0800

    v1

--- database/configDB.php
+++ database/configDB.php
@@ -3,12 +3,12 @@
 function connDB()
 {
 
-    $url         = "DATABASE_URL = postgres://buulugaexiflfy:c9232749191e959e79ce0a0fd1b806a7bb835354b6d391dfd54b596c2a750f92@ec2-54-235-158-17.compute-1.amazonaws.com:5432/d8h7sjoiqa1il";
-    $host        = "DB_HOST = ec2-54-235-158-17.compute-1.amazonaws.com";
-    $port        = "DB_PORT = 5432";
-    $dbname      = "DB_DATABASE = d8h7sjoiqa1il";
-    $user        = "DB_USERNAME = buulugaexiflfy";
-    $password    = "DB_PASSWORD = c9232749191e959e79ce0a0fd1b806a7bb835354b6d391dfd54b596c2a750f92";
+    $url         = "url = postgres://buulugaexiflfy:c9232749191e959e79ce0a0fd1b806a7bb835354b6d391dfd54b596c2a750f92@ec2-54-235-158-17.compute-1.amazonaws.com:5432/d8h7sjoiqa1il";
+    $host        = "host = ec2-54-235-158-17.compute-1.amazonaws.com";
+    $port        = "port = 5432";
+    $dbname      = "dbname = fyp";
+    $user        = "user = buulugaexiflfy";
+    $password    = "password = c9232749191e959e79ce0a0fd1b806a7bb835354b6d391dfd54b596c2a750f92";
     //$credentials = "user = buulugaexiflfy password=c9232749191e959e79ce0a0fd1b806a7bb835354b6d391dfd54b596c2a750f92";
 
     $db = pg_connect("$url $host $port $dbname $user $password");
