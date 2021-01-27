32c9e86f04a5b81f46e084611d392f8246f6d56c
Muhamad Luqman <muhamadluqmanhakim69@gmail.com>
2021-01-27 17:41:16 +0800

    .

--- bot.php
+++ bot.php
@@ -53,7 +53,7 @@ $botman->hears("/book_appointment", function (BotMan $bot) {
     $bot->reply("Format Booking:\n\n/book [Name]_[IC]_[Phone]_[Appointment Date]\n\n*Without brackets []");
 });
 
-$botman->hears("/book {name}_{ic}_{phone}_{bookdate}", function (Botman $bot, $name, $ic, $phone, $bookdate) {
+$botman->hears("/book {name}_{ic}_{phone}_{bookdate}", function ($bot, $name, $ic, $phone, $bookdate) {
     $user = $bot->getUser();
     $firstname = $user->getFirstName();
     $id_user = $user->getId();
