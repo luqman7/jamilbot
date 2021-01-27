c30c408809148cbc2cfee2fbffc34af74e1589e0
Muhamad Luqman <muhamadluqmanhakim69@gmail.com>
2021-01-27 18:13:38 +0800

    .

--- command/bookAppointment.php
+++ command/bookAppointment.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . '/../database/configDB.php');
 function bookApt($id_user, $name, $ic, $phone, $bookdate)
 {
 
-    $querybookApt = "INSERT INTO appointments (id, name, ic, phone, bookdate, id_user) VALUES (NULL, '$name', '$ic', '$phone', '$bookdate', '$id_user')";
+    $querybookApt = "INSERT INTO appointments (name, ic, phone, bookdate, id_user) VALUES ('$name', '$ic', '$phone', '$bookdate', '$id_user')";
     $resultQueryInsert  = pg_query(connDB(), $querybookApt);
 
     if ($resultQueryInsert) {
