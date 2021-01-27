ce392e850441de94b00cb46468238723f019136d
Muhamad Luqman <muhamadluqmanhakim69@gmail.com>
2021-01-27 18:24:41 +0800

    v2

--- command/delBooking.php
+++ command/delBooking.php
@@ -5,12 +5,13 @@ require_once(__DIR__ . '/../database/configDB.php');
 function delBooking($id_user, $id)
 {
     $queryFlagDelete = "SELECT id FROM appointments WHERE id_user = $id_user AND id=$id";
-    $resultQueryFlag  = mysqli_query(connDB(), $queryFlagDelete);
+    $resultQueryFlag  = pg_query(connDB(), $queryFlagDelete);
+    $rows = pg_num_rows($resultQueryFlag);
 
     $message = "";
 
     // jika data ada maka dihapus sesuai benar id nya
-    if ($resultQueryFlag->num_rows > 0) {
+    if ($rows > 0) {
 
         $queryDeleteBooking = "DELETE FROM appointments WHERE id_user = $id_user AND id=$id";
         $resultQueryDelete  = mysqli_query(connDB(), $queryDeleteBooking);
